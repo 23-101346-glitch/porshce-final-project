5fbeb6ff0ce47448c803c78cb3659f7b1da43e58
Janna khaled <23-101346@students.eui.edu.eg>
2025-12-17 10:33:00 +0200

    s5 color changing

--- home.js
+++ home.js
@@ -8,3 +8,24 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+const modelViewer = document.querySelector('#carModel');
+const colorButtons = document.querySelectorAll('.colorbt');
+
+modelViewer.addEventListener('load', () => {
+    const materials = modelViewer.model.materials;
+
+    colorButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const color = btn.dataset.color;
+
+            materials.forEach(material => {
+                material.pbrMetallicRoughness.setBaseColorFactor([
+                    parseInt(color.substr(1,2), 16) / 255,
+                    parseInt(color.substr(3,2), 16) / 255,
+                    parseInt(color.substr(5,2), 16) / 255,
+                    1
+                ]);
+            });
+        });
+    });
+});
